d08d4f72cd1d409bba6ccbb24dfab5f1eff17559
magnus.lj.zetterberg@gmail.com <magnus.lj.zetterebrg@gmail.com>
2016-12-23 15:03:00 +0100

    Initial V1.0.10

---
+++ MyHello/src/MyHello.java
@@ -0,0 +1,15 @@
+
+public class MyHello {
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello world version 1.0.6");
+		System.out.println("Hello world version 1.0.7");
+		System.out.println("Hello world version 1.0.8");
+		System.out.println("Hello world version 1.0.9");
+		System.out.println("Hello world version 1.0.10"); 
+
+	}
+	
+}
